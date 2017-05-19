532f35cfaeb3e40bf1be64e0b94f8a89a57e5fee
Kendall Fuhrman <kendall.fuhrman@gmail.com>
2017-05-19 21:24:30 +0300

    Initial Commit

---
+++ iXperience Pre-workTests/iXperience_Pre_workTests.swift
@@ -0,0 +1,36 @@
+//
+//  iXperience_Pre_workTests.swift
+//  iXperience Pre-workTests
+//
+//  Created by Kendall Fuhrman on 5/19/17.
+//  Copyright © 2017 Kendall Fuhrman. All rights reserved.
+//
+
+import XCTest
+@testable import iXperience_Pre_work
+
+class iXperience_Pre_workTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
